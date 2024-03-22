33e0cc1d7d99aace3aff88b93c0425173f44d6bb
Luiz de Freitas <luizdefreitas10@gmail.com>
2024-03-22 15:50:22 -0300

    feat: add correct links

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import logoedsfooter from '../../../../public/logoedsfooter.png'
 import twittericon from '../../../../public/twittericon.png'
 import youtubeicon from '../../../../public/youtubeicon.png'
 import instagramicon from '../../../../public/instagramicon.png'
-import Link from 'next/link'
+import { Link } from '@nextui-org/react'
 
 export default function Footer() {
   return (
@@ -13,13 +13,25 @@ export default function Footer() {
         @2024 Copyright - Esportedasorte
       </p>
       <div className="flex space-x-3 cursor-pointer">
-        <Link href={''} className="cursor-pointer">
+        <Link
+          href="https://twitter.com/EsportesDaSorte"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <Image src={twittericon} alt="twittericon" />
         </Link>
-        <Link href={''} className="cursor-pointer">
+        <Link
+          href="https://www.youtube.com/@esportesdasorteoficial"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <Image src={youtubeicon} alt="youtubeicon" />
         </Link>
-        <Link href={''} className="cursor-pointer">
+        <Link
+          href="https://www.instagram.com/esportesdasorte/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <Image src={instagramicon} alt="instagramicon" />
         </Link>
       </div>
