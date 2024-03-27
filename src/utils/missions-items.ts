191e2b04e02a81a500d793dc68f1264753c5d9f7
Luiz de Freitas <luizdefreitas@MacBook-Pro-de-Luiz.local>
2024-03-27 11:00:38 -0300

    feat: mission points

--- src/utils/missions-items.ts
+++ src/utils/missions-items.ts
@@ -9,14 +9,14 @@ export const missions = [
   {
     id: 2,
     title: 'Missões Nível 2',
-    points: 10,
+    points: 25,
     description: `O time com mais seguidores em comum com a Esportes da Sorte ganha 50 bancas de R$10,00 para distribuir com o time.`,
     link: 'https://www.esportesdasorte.com',
   },
   {
     id: 3,
     title: 'Missões Nível 3',
-    points: 10,
+    points: 25,
     description: `O time que arrecadar mais cadastros com primeiro depósito de até R$1,00 ganhará R$1.000,00 reais de banca da Esportes da Sorte (a partir de 300 depósitos).`,
     link: 'https://www.esportesdasorte.com',
   },
