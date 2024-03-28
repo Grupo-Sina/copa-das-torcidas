b2aee4ef18cfbe7de70448ec4c0ff44b8284fba2
Luiz de Freitas <luizdefreitas@MacBook-Pro-de-Luiz.local>
2024-03-27 21:10:21 -0300

    fix: active mission

--- src/utils/missions-items.ts
+++ src/utils/missions-items.ts
@@ -2,22 +2,22 @@ export const missions = [
   {
     id: 1,
     title: 'Missões Nível 1',
-    points: 10,
-    description: `O comentário mais curtido em uma postagem da Copa das Torcidas ganha R$ 50,00 de banca na Esportes da Sorte.  A mecânica terá um tempo estabelecido!`,
-    link: 'https://www.esportesdasorte.com',
-  },
-  {
-    id: 2,
-    title: 'Missões Nível 2',
     points: 25,
-    description: `O time com mais seguidores em comum com a Esportes da Sorte ganha 50 bancas de R$10,00 para distribuir com o time.`,
+    description: `O time com mais seguidores em comum com a Copa das Torcidas ganha 50 bancas de R$10,00 pra distribuir com os torcedores.`,
     link: 'https://www.esportesdasorte.com',
   },
-  {
-    id: 3,
-    title: 'Missões Nível 3',
-    points: 25,
-    description: `O time que arrecadar mais cadastros com primeiro depósito de até R$1,00 ganhará R$1.000,00 reais de banca da Esportes da Sorte (a partir de 300 depósitos).`,
-    link: 'https://www.esportesdasorte.com',
-  },
+  // {
+  //   id: 2,
+  //   title: 'Missões Nível 2',
+  //   points: 25,
+  //   description: `O time com mais seguidores em comum com a Esportes da Sorte ganha 50 bancas de R$10,00 para distribuir com o time.`,
+  //   link: 'https://www.esportesdasorte.com',
+  // },
+  // {
+  //   id: 3,
+  //   title: 'Missões Nível 3',
+  //   points: 25,
+  //   description: `O time que arrecadar mais cadastros com primeiro depósito de até R$1,00 ganhará R$1.000,00 reais de banca da Esportes da Sorte (a partir de 300 depósitos).`,
+  //   link: 'https://www.esportesdasorte.com',
+  // },
 ]
