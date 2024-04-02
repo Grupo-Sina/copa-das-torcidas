b2ff8390f0f951154d5dd2ecc8e220a5d7870837
Yuri Muniz <y.munizgonzalez@gmail.com>
2024-04-02 15:21:53 -0300

    add link faq

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -1,14 +1,17 @@
-type ButtonProps = JSX.IntrinsicElements['button'] & {
+import { Button as ButtonUI, ButtonProps } from '@nextui-org/react'
+
+type ButtonPropsUI = ButtonProps & {
   title: string
 }
 
-export default function Button({ title, ...rest }: ButtonProps) {
+export default function Button({ title, ...rest }: ButtonPropsUI) {
   return (
-    <button
+    <ButtonUI
+      as="button"
       {...rest}
       className="font-headingExtraBold py-[12px] sm:px-[32px] px-[24px] text-tiny text-[10px] md:text-[12px] lg:text-[16px] rounded-[100px] bg-[#00E46F]  text-[#001F6D]"
     >
       {title}
-    </button>
+    </ButtonUI>
   )
 }
