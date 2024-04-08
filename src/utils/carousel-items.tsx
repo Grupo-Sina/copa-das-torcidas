ba44c72b5d34fcdd6d392a15bcc7aeaa02c1d2fc
Yuri Muniz <y.munizgonzalez@gmail.com>
2024-04-08 10:37:10 -0300

    new item carrosel

--- src/utils/carousel-items.tsx
+++ src/utils/carousel-items.tsx
@@ -14,6 +14,11 @@ export const carouselItemDesktop = [
     alt: 'banner eds',
     link: 'https://m.esportesdasorte.com/ptb/games/casino/detail/normal/23109',
   },
+  {
+    src: '/spaceman-large.png',
+    alt: 'banner eds',
+    link: 'https://www.esportesdasorte.com/ptb/games/casino/detail/normal/19364',
+  },
 ]
 
 export const carouselItemMobile = [
@@ -31,5 +36,10 @@ export const carouselItemMobile = [
     src: '/penalti-mobile.png',
     alt: 'banner eds',
     link: 'https://m.esportesdasorte.com/ptb/games/casino/detail/normal/23109',
+  },
+  {
+    src: '/spaceman-mobile.png',
+    alt: 'banner eds',
+    link: 'https://www.esportesdasorte.com/ptb/games/casino/detail/normal/19364',
   },
 ]
