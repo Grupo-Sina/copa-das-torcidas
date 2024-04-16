4239736887a526d9ab511ee7b06c2b8e0093d2e5
Luiz de Freitas <luizdefreitas@MacBook-Pro-de-Luiz.local>
2024-04-16 18:19:16 -0300

    feat: initializing home

--- src/app/(bg-dark-blue)/page.tsx
+++ src/app/(bg-dark-blue)/page.tsx
@@ -12,33 +12,45 @@ export default function Home() {
   //     : '/bannerclubedasorte.png'
 
   return (
-    <main className="w-full flex-1 flex flex-col items-center justify-evenly overflow-auto lg:p-8 py-8 px-2 gap-5 h-full">
+    <main className="w-full flex-1 flex flex-col items-center desktop:justify-center justify-around xl:justify-evenly overflow-auto lg:p-8 py-8 px-2 gap-5 h-full relative">
+      <CarouselLargeComponent />
+      <CarouselMobileComponent />
       <div
-        className="overflow-hidden flex flex-col lg:flex-row items-center justify-center 2xl:gap-16 bg-white w-[85%] p-4 px-4 rounded-xl h-auto"
+        className="flex flex-col lg:flex-row items-center justify-end gap-16 bg-white w-[85%] p-4 px-4 rounded-xl h-auto xl:relative"
         style={{ backgroundImage: "url('/lightbgdiv.png')" }}
       >
         <Image
           src={'/escudocopadastorcidas.png'}
           alt="escudo copa das torcidas"
-          width={337}
-          height={361}
+          width={369}
+          height={394}
           quality={100}
           priority={true}
-          className="w-[168px] h-[180px] xl:h-[321px] xl:w-[287px]"
+          className="hidden xl:flex xl:absolute left-[-70px] bottom-[-80px] desktop:h-[394px] desktop:w-[369px]"
         />
-        <div className="flex flex-col flex-grow h-full max-w-[1222px] my-8 lg:my-0 px-4 2xl:px-0">
-          <h1 className="text-shadow mb-4 text-center text-[32px] xl:text-left text-bold desktop:text-[65px] lg:text-[50px] text-[#001F6D] font-cloverGroteskBlack">
+        <Image
+          src={'/escudocopadastorcidas.png'}
+          alt="escudo copa das torcidas"
+          width={168}
+          height={180}
+          quality={100}
+          priority={true}
+          className="absolute top-[300px] right-[43%] h-[180px] w-[168px] xl:hidden"
+        />
+
+        <div className="flex flex-col h-full my-8 lg:my-0 px-4 pt-20 xl:pt-0 2xl:px-0 w-[100%] xl:w-[70%] 2xl:w-[75%]">
+          <h1 className="text-shadow text-center xl:text-left mb-4 text-[32px] text-bold desktop:text-[65px] lg:text-[50px] text-[#001F6D] font-cloverGroteskBlack">
             COPA DAS TORCIDAS
           </h1>
 
-          <p className="desktop:text-[33px] text-[18px] text-[#001F6D] font-robotoRegular text-justify">
+          <p className="w-full desktop:text-[33px] text-[24px] text-[#001F6D] font-robotoRegular text-justify items-center">
             Foi dada a largada na{' '}
             <span className="font-bold font-robotoBold">COPA DAS TORCIDAS</span>
             , que vai em busca da torcida mais apaixonada para premiar somente
             os de fé da Várzea!
           </p>
           <br />
-          <p className="font-robotoRegular text-[18px] text-[#001F6D] text-justify">
+          {/* <p className="font-robotoRegular text-[18px] text-[#001F6D] text-justify">
             Como a Esportes da Sorte não deixa a brincadeira ficar chata para
             ninguém, a procura pela torcida com o grito mais forte vai vir
             acompanhada de premiações semanais, com bancas na plataforma
@@ -46,36 +58,9 @@ export default function Home() {
             personalizados e o seu time ainda vai poder lutar pelo grande
             prêmio! Assim, a corrida até o título fica ainda mais emocionante
             para você!
-          </p>
+          </p> */}
         </div>
       </div>
-      <CarouselLargeComponent />
-      <CarouselMobileComponent />
-      {/* <Link
-        className="w-[85%] rounded-lg"
-        href="https://m.esportesdasorte.com/ptb/pages/clube_eds"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <Image
-          src={'/bannerclubedasorte.png'}
-          alt="banner eds"
-          width={1640}
-          height={276}
-          quality={100}
-          priority={true}
-          className="mx-auto mb-8 lg:mb-0 h-[210px] w-[100%] hidden md:block "
-        />
-        <Image
-          src={'/mobile-bannerclubedasorte.png'}
-          alt="banner eds"
-          width={1640}
-          height={276}
-          quality={100}
-          priority={true}
-          className="mx-auto mb-8 lg:mb-0 h-[210px] w-[100%] block md:hidden"
-        />
-      </Link> */}
     </main>
   )
 }
