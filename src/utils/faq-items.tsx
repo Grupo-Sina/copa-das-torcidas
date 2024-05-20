434118804e18761ae51a5cc72b63aa9b08a3a673
Yuri Muniz <y.munizgonzalez@gmail.com>
2024-05-20 15:49:16 -0300

    faq-new-layout-dev

--- src/utils/faq-items.tsx
+++ src/utils/faq-items.tsx
@@ -6,12 +6,22 @@ export const faqItems = [
   {
     title: 'Como participo das missões?',
     descriptions: [
-      'É facin facin! É só você ficar ligado no Instagram @copadatorcidas que as missões são publicadas lá. Mas fica ligado que para poder ganhar, você tem que ter um cadastro na Esportes da Sorte.',
       <>
-        E se você ainda não tiver um cadastro, não tem problema! Basta procurar
-        o nome do seu time na aba "TIMES" aqui do site, clicar no link da sua
-        torcida, fazer o cadastro rapidinho. E o melhor, fazendo isso é mais um
-        jeito de você acumulara pontos para a sua torcida!
+        É facin facin! É só você ficar ligado no{' '}
+        <b>Instagram @copadatorcidas</b> que as missões são publicadas lá.{' '}
+        <b>
+          Mas fica ligado que para poder ganhar, você tem que ter um cadastro no
+          Esportes da Sorte.
+        </b>
+      </>,
+      <>
+        E se você ainda não tiver um cadastro, não tem problema!{' '}
+        <b>
+          Basta procurar o nome do seu time na aba "TIMES" aqui do site, clicar
+          no link da sua torcida, fazer o cadastro rapidinho.
+        </b>{' '}
+        E o melhor, fazendo isso é mais um jeito de você acumulara pontos para a
+        sua torcida!
       </>,
     ],
   },
