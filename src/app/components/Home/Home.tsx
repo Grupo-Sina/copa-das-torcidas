e2e793aff3699a7eb1a04161b544b899444f7c03
Yuri Muniz <y.munizgonzalez@gmail.com>
2024-06-04 12:56:04 -0300

    volume control

--- src/app/components/Home/Home.tsx
+++ src/app/components/Home/Home.tsx
@@ -6,13 +6,14 @@ import HomeContent from '../HomeContent/HomeContent'
 import dynamic from 'next/dynamic'
 import { Button, Spinner } from '@nextui-org/react'
 import { useAppContext } from '@/context/AppContext'
+import VolumeInput from '../VolumeInput'
 
 const HomeVideo = dynamic(() => import('../HomeVideo/HomeVideo'), {
   ssr: false,
 })
 export default function HomeComponent() {
   const windowWidth = useWindowWidth()
-  const { videoReady } = useAppContext()
+  const { videoReady, setShouldMutedVideo, setVolumeVideo } = useAppContext()
   const [shouldShowVideo, setShouldShowVideo] = useState(true)
   const isLargeScreen = !!(windowWidth && windowWidth > 1024)
 
@@ -27,18 +28,25 @@ export default function HomeComponent() {
   return (
     <div className="flex justify-center items-center h-full">
       {shouldShowVideo ? (
-        <div className="">
+        <div>
           <HomeVideo />
           {videoReady ? (
-            <div>
+            <div className="flex w-full items-center justify-center gap-3">
               <Button
-                onClick={() => setShouldShowVideo(false)}
+                onClick={() => {
+                  setShouldShowVideo(false)
+                  setShouldMutedVideo(true)
+                  setVolumeVideo(0)
+                }}
                 rel="noopener noreferrer"
                 radius="full"
-                className="bg-[#00E46F] lg:bottom-auto bottom-6 lg:mt-[-120px]  text-[#003B9C]  flex py-3 px-8 font-headingBold text-[16px] absolute  left-1/2 transform -translate-x-1/2 z-[3]"
+                className="bg-[#00E46F]  font-extrabold px-8 h-10  lg:mt-[-180px]  text-[#003B9C]  flex  font-headingBold text-[24px]   z-[3]"
               >
                 SAIBA MAIS
               </Button>
+              <div className="lg:bottom-auto bottom-6 lg:mt-[-180px]  z-[3]">
+                <VolumeInput />
+              </div>
             </div>
           ) : (
             <Spinner size="lg" color="primary" />
